5fa06dfb7ea6a8a69cee1d184637f020e6566d48
AdrianaSchmit <schmit.adriana@gmail.com>
2022-02-23 21:39:06 +0000

    Added shopping bag toast and messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,7 +14,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     color = None
@@ -26,16 +26,20 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if color in cart[item_id]['items_by_color'].keys():
                 cart[item_id]['items_by_color'][color] += quantity
+                messages.success(request, f'Updated  {color.upper()} {product.name} color quantity to {cart[item_id]["items_by_color"][color]}')
             else:
                 cart[item_id]['items_by_color'][color] = quantity
+                messages.success(request, f'Added  {product.name}  color {color.upper()} to your cart')
         else:
             cart[item_id] = {'items_by_color': {color: quantity}}
+            messages.success(request, f'Added {product.name}  color {color.upper()} to your cart')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
+            messages.success(request, f' {product.name} quantity was updated to {cart[item_id]}')
         else:
             cart[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your cart')
+            messages.success(request, f' {product.name} was added to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)  
@@ -44,6 +48,7 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     color = None
     if 'product_color' in request.POST:
@@ -53,15 +58,19 @@ def adjust_cart(request, item_id):
     if color:
         if quantity > 0:
             cart[item_id]['items_by_color'][color] = quantity
+            messages.success(request, f'Updated {product.name} color {color.upper()}  quantity to {cart[item_id]["items_by_color"][color]}')
         else:
             del cart[item_id]['items_by_color'][color]
             if not cart[item_id]['items_by_color']:
                 cart.pop(item_id)
+                messages.success(request, f'Removed {product.name} color {color.upper()}  from your cart')
     else:
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -71,6 +80,7 @@ def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         color = None
         if 'product_color' in request.POST:
             color = request.POST['product_color']
@@ -80,13 +90,16 @@ def remove_from_cart(request, item_id):
             del cart[item_id]['items_by_color'][color]
             if not cart[item_id]['items_by_color']:
                 cart.pop(item_id)
+                messages.success(request, f'Removed {product.name}  color {color.upper()} from your cart')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
 
 
